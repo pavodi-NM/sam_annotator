9dca5843f158b7297e6006b4c7f7f492a5ed0b7b
pavodi-NM <pmanindoyi@gmail.com>
2025-04-13 22:55:26 +0900

    Alpha stable 2.1a10

--- sam_annotator/__init__.py
+++ sam_annotator/__init__.py
@@ -5,6 +5,6 @@ SAM Annotator - A tool for image annotation using Segment Anything Model (SAM)
 """
 
 
-__version__ = "0.2.2.dev9"
+__version__ = "0.2.1.a10"
 # dev __version__ = "0.2.2.dev8"
 # prod __version__ = "0.2.1a8"
