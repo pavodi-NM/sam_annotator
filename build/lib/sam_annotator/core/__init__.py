8f79b4d2c11afa17b2af997bcba040e452862f8c
pavodi-NM <pmanindoyi@gmail.com>
2025-04-26 13:41:50 +0900

    mkdocs files generated locally for test purpose are removed

--- build/lib/sam_annotator/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .annotator import SAMAnnotator
-from .weight_manager import SAMWeightManager
-from .predictor import SAM1Predictor, SAM2Predictor
-
-__all__ = ['SAMAnnotator', 'SAMWeightManager', 'SAMPredictor', 'SAM2Predictor']
--- build/lib/sam_annotator/ui/__init__.py
+++
@@ -1,4 +0,0 @@
-from .window_manager import WindowManager
-from .event_handler import EventHandler
-
-__all__ = ['WindowManager', 'EventHandler']
